e38b4a1eb340be941f9840b587d09f615c758fea
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-12-11 15:18:28 +0530

    variables

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//variable declaration
+let bookName;
+let movieName,Moviecharac;
+
+//variable initialization
+bookName = "Harry Potter";
+
+let author = "jk rowling" , publisher ="WB";
+//bookName = "Monk"
+console.log("book name is : ",bookName)
