a605352aea8e240a89e630f8be43f71e6bfd0ae4
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-12-14 15:53:11 +0530

    Update loops.js

---
+++ loops.js
@@ -0,0 +1,43 @@
+// let num=1
+
+// while(num<10){
+//     console.log(num)
+//     num++
+// }
+
+// do{
+//     console.log(num)
+//     num++
+// }while(num<10)
+
+
+
+// for(let num=1;num<=10;num++){
+//     console.log(num)
+// }
+
+
+//factorial
+// let i;
+// let num=10
+// let fact=1;
+
+// for(i=1;i<=num;i++){
+//     fact=fact*i
+// }
+// console.log(fact)
+
+
+//sum of n numbers
+
+let num=5
+let total=0
+
+for(i=1;i<=num;i++){
+    total=total+i
+}
+
+console.log(total)
+
+
+
