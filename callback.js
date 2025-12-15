15dc16fb724d7f5d41dc8fc597bff7ddffa0aa66
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-12-15 17:21:54 +0530

    12/15

---
+++ callback.js
@@ -0,0 +1,8 @@
+function operation(op,a,b){
+    return op(a,b)
+}
+
+let add = (a,b)=>a+b
+let sub = (a,b)=>a-b
+
+console.log(operation(sub,2,5))
---
+++ functions.js
@@ -0,0 +1,33 @@
+// function sum(a,b){ //function declaration
+//     let total = a+b
+//     return total
+// }
+
+// let number = sum(10,20) //function calling
+// console.log(number)
+
+
+// function decideOdd_even(num){
+//     let str
+//     if(num%2==0){
+//         str="Even number"
+//     }
+//     else{
+//         str="Odd number"
+//     }
+//     return str
+// }
+
+// let value = decideOdd_even(19)
+// console.log(value)
+
+
+
+// let a = 11
+// let OddEven = (a) => a%2==0? "Even":"Odd"
+// console.log(OddEven(14))
+
+
+
+let mul = (num)=> num*5
+console.log(mul(5))
