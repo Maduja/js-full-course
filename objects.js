bf9e45852f3701163414ee5835eb5b2185bca648
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-12-15 19:41:40 +0530

    Update objects.js

---
+++ objects.js
@@ -0,0 +1,44 @@
+// //object declaring
+
+// let x ={}
+// let EngineOptions = "petrol, Diesel"
+
+// let car ={
+//     name:'Nexon',
+//     brand:'Tata',
+//     driveTrain:EngineOptions+', ev',
+//     "car color":'red',
+//     price:15,
+//     onRoadPrice(){
+//         return this.price+2
+//     }
+// }
+
+// // console.log(car)
+// // console.log(car["car color"])
+// // //console.log(car.name)
+
+
+// // console.log(car)
+// // //delete car.price
+
+// // car['modelYear']=2024
+// // console.log(car)
+
+// //car['price'] = 18
+// car.price=18
+// console.log(car.onRoadPrice())
+
+
+let user={}
+
+user={
+    fname:"maduja"
+}
+
+console.log(user.fname)
+user['lname']='abhayasundara'
+
+console.log(user)
+delete user.lname
+console.log(user)
